c91c20fff5389e61304ac79abb2e2c608ea673d9
Lucas Gomes <msilva_lucas@sicredi.com.br>
2023-05-07 01:51:07 -0300

    feat: aula sobre boas práticas | variavies

--- _explicacoes/a1.1.js
+++ _explicacoes/a1.1.js
@@ -10,3 +10,19 @@ const userFirstInputNumber = 10;
 const userSecundInputNumber = 20;
 
 console.log(userFirstInput + userSecundInput);
+
+// ===============
+var inputDoUsuario;
+// No Browser
+// - Pode ser uma string
+// - Pode ser um elemento do DOM (Campo de busca do google)
+const inputDoUsuario = document.querySelector('input').value;
+const $inputDoUsuario = document.querySelector('input'); // Convensão, isso indica que estamos selecionando um element da DOM
+
+// BOOLEANOS
+// <input value="">
+document.querySelector('input').hasAttribute('value'); // true | false
+// has, is (tem algo, é algo)
+if (hasAttribute) {} // tem o atributo ?
+if (isEmpty) {} // é vazio ?
+if (!hasName) {} // Não tem name ?
