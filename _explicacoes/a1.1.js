48ed9060e141a66b0c5ca4275e1cd49152e1ba2d
Lucas Gomes <msilva_lucas@sicredi.com.br>
2023-05-07 01:49:29 -0300

    feat: aula sobre boas práticas | variavies

---
+++ _explicacoes/a1.1.js
@@ -0,0 +1,12 @@
+// [Dar bons nomes de variáveis]
+var n1 = 10;
+var n2 = 20;
+
+console.log(n1 + n2);
+
+// Como devs, o que mais fazemos é LER código!
+// Contexto é extremamente importante!
+const userFirstInputNumber = 10;
+const userSecundInputNumber = 20;
+
+console.log(userFirstInput + userSecundInput);
